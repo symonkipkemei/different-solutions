4a3012039d6b42e0b3a07175a71ebd193a5a2a04
martin-martin <breuss.martin@gmail.com>
2019-02-25 07:31:32 +0800

    add fifth solution

---
+++ solution_05.py
@@ -0,0 +1,14 @@
+'''
+Write a script that sorts a list of tuples based on the number value in the tuple.
+For example:
+
+unsorted_list = [('first_element', 4), ('second_element', 2), ('third_element', 6)]
+sorted_list = [('second_element', 2), ('first_element', 4), ('third_element', 6)]
+
+'''
+
+unsorted_list = [('first_element', 4), ('second_element', 2), ('third_element', 6)]
+sorted_list = []
+
+sorted_list = sorted(unsorted_list, key=lambda x: x[1])
+print(sorted_list)
